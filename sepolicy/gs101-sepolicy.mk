3032fe4220ab3180aee3365666c93d46191c8ad2
Pyrtle93 <xxpyrtlexx93@gmail.com>
2023-10-16 14:32:32 -0400

    gs101: some sepolicy updates
    Took a few things from https://github.com/Pyrtle93/device_google_gs101-sepolicy

--- sepolicy/gs101-sepolicy.mk
+++ sepolicy/gs101-sepolicy.mk
@@ -1,3 +1,15 @@
+# ConnectivityThermalPowerManager
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power_manager
+
+# ramdump
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump
+
+# twoshay
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
+
+# google_battery service
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
 # sepolicy that are shared among devices using whitechapel
 BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/whitechapel/vendor/google
 
@@ -23,20 +35,19 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/priv
 #   Dauntless (uses Citadel policy currently)
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/citadel
 
-#   Wifi
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_ext
-
 #   PowerStats HAL
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 
 # sscoredump
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/sscoredump
 
-# Sniffer Logger
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
-
-# Wifi Logger
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_logger
-
 # Public
 PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/public
+
+# pKVM
+ifeq ($(TARGET_PKVM_ENABLED),true)
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/pkvm
+endif
+
+# Health HAL
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/health
